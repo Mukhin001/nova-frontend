dcb40931f224873f31896a38997c5c5bd588b8d5
Mukhin001 <purgen.igor@yandex.ru>
2026-02-06 17:02:29 +0300

    url https://nova-frontend-sigma.vercel.app

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -8,7 +8,7 @@ interface GreetingResponse {
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "nova-backend-production-bf25.up.railway.app",
+    baseUrl: "https://nova-frontend-sigma.vercel.app",
     credentials: "include", // <-- ДЛЯ COOKIE
   }),
   tagTypes: ["User", "Weather", "News", "Feed"],
