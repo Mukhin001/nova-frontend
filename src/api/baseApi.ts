939c75e67e104cabb12b46e2bf748c0d126691bc
Mukhin001 <purgen.igor@yandex.ru>
2026-02-08 09:58:39 +0300

    сделали дерево git с тремя репозиториями

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -9,12 +9,13 @@ export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:3000",
+    // baseUrl: "/api",
     credentials: "include", // <-- ДЛЯ COOKIE
   }),
   tagTypes: ["User", "Weather", "News", "Feed"],
   endpoints: (builder) => ({
     getGreetingServer: builder.query<GreetingResponse, void>({
-      query: () => "",
+      query: () => "/greet",
     }),
     getMe: builder.query<LoginResponse, void>({
       query: () => "/user/me",
